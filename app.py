7c9da3c966f69c7edada9793d4bbbb8d513aacec
Sonika Deshwal <151389394+sonikadeshwal@users.noreply.github.com>
2026-03-16 00:19:49 +0530

    Update placeholder for name input field

--- app.py
+++ app.py
@@ -1049,7 +1049,7 @@ def page_setup():
             st.markdown('<div class="card">', unsafe_allow_html=True)
             st.markdown('<p class="slabel">🔑 Credentials & Role</p>', unsafe_allow_html=True)
 
-            name = st.text_input("Your Name (optional)", placeholder="e.g. Aditya Singh",
+            name = st.text_input("Your Name (optional)", placeholder="e.g. Sonika Deshwal",
                                  value=st.session_state.candidate_name)
             st.session_state.candidate_name = name
 
